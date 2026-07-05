2fdc7805ef26489d011b3b648b1668d8bf63a6fb
delishamahapatro <delishamahapatro@gmail.com>
2026-07-05 17:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (23.90%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -9,5 +9,19 @@ class Solution {
             }
         }
         return m;
+
+        /*
+        int i = 0;
+        int n = nums.length;
+        while(i < n) {
+           if(nums[i] == val) {
+              nums[i] = nums[n - 1];
+              n--;
+           } else {
+              i++;
+           }
+        }
+
+        */
     }
 }
