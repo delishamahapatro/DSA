519d1d788b1733d96300a83c007db2f9e356bd67
delishamahapatro <delishamahapatro@gmail.com>
2026-07-05 17:53:15 +0530

    Time: 2 ms (92.08%) | Memory: 48.1 MB (6.72%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        /*
+        we have 2 different approach's
+        1 -> check using for loop and if its not 0 put it in array and when 0 pops in using while loop create another array, put it in the end
+        2 -> 2 pointer approach, where in we check the element if != 0 use temp and swap with next element
+        */
+        
+        int k = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        while ( k < nums.length) {
+            nums[k] = 0;
+            k++;
+        }
+    }
+}
