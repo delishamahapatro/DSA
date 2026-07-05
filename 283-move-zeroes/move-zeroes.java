5690b616f7a1a4f71c99dfc2686de3bc388277ce
delishamahapatro <delishamahapatro@gmail.com>
2026-07-05 17:56:02 +0530

    Time: 2 ms (92.08%) | Memory: 47.8 MB (56.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -5,7 +5,8 @@ class Solution {
         1 -> check using for loop and if its not 0 put it in array and when 0 pops in using while loop create another array, put it in the end
         2 -> 2 pointer approach, where in we check the element if != 0 use temp and swap with next element
         */
-        
+
+        /*
         int k = 0;
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != 0) {
@@ -16,6 +17,16 @@ class Solution {
         while ( k < nums.length) {
             nums[k] = 0;
             k++;
-        }
+            */
+
+            int j = 0;
+            for(int i = 0; i < nums.length; i++) {
+                if(nums[i] != 0) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    j++;
+                }
+            }
     }
 }
