d44d67e1dc28157065ffab94ab7afece36899351
delishamahapatro <delishamahapatro@gmail.com>
2026-07-07 12:04:18 +0530

    Time: 2 ms (99.27%) | Memory: 44.6 MB (48.50%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(arr[left])) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(arr[right])) {
+                right--;
+            }
+
+            if (Character.toLowerCase(arr[left]) != Character.toLowerCase(arr[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+        //O(n) and O(n) - time & space complexity
+    }
+}
