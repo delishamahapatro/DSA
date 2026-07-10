8f820ded02d121167f0edd37640af5e666d89f4b
delishamahapatro <delishamahapatro@gmail.com>
2026-07-10 18:38:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (78.43%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        //2 ways we can solve this, say using sqrt method or binary search
+
+        //using method , O(1) time & space complexity
+
+        return (int)Math.sqrt(x);
+    }
+}
